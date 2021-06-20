64f73e11faf00144bc3b79d0f303214a0771eb7d
Ike Isenhour <thirdike@gmail.com>
2021-06-20 13:24:35 -0400

    Initial Commit

---
+++ script.js
@@ -0,0 +1,68 @@
+/*
+let string1="This"
+let string2=" is"
+let string3=' a'
+let string4=' test'
+console.log(string1+string2+string3+string4);
+
+if (2>1) {
+    alert(0>1)
+    alert(true||false)
+    alert(true && true)
+}
+
+output(string1)
+*/
+
+/*
+function fizzBuzz (a) {
+    let counter=0
+    while (counter<a) {
+        counter=counter+1
+        if (counter%3===0) {
+            console.log( "Fizz")
+        }
+        else if (counter%5===0) {
+            console.log("Buzz")
+        }
+        else {
+            console.log(counter)
+        }
+    }
+}
+fizzBuzz(15)
+
+function fizzBuzz (a) {
+    let counter=0
+    let list=[]
+    while (counter<a) {
+        counter=counter+1
+        if (counter%3===0) {
+            list.push("Fizz")
+        }
+        else if (counter%5===0) {
+            list.push("Buzz")
+        }
+        else {
+            list.push(counter)
+        }
+        console.log(list)
+    }
+}
+fizzBuzz(15)
+
+let number=parseInt(prompt("Please enter a number"))
+for (i=1; i<=number; i++) {
+    let list=[]
+    if (i%3===0) {
+        list.push("Fizz")
+    }
+    else if (i%5===0) {
+        list.push("Buzz")
+    }
+    else {
+        list.push(i)
+    }
+    console.log(list)
+}
+*/
